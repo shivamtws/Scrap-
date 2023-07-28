b43656933741887f9c5c145b7593f41a3d73872f
shivampateltws <shivampatel.tws@gmail.com>
2023-07-28 15:34:48 +0530

    fix some issue

--- myapp/views.py
+++ myapp/views.py
@@ -3,27 +3,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 import openai
-API_KEY = ""
+API_KEY = "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"
 
 def content(text):
+    # print("Text", text)
     openai.api_key = API_KEY
 
-    prompt = f"Create an Instagram caption:\n{text}\n\nCaption:"
-    response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
+    prompt = f"Create an Instagram caption without hashtags according to this text: \n{text} and make sure the words approx 50"
+    
+
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=50)
 
     if 'choices' in response and len(response['choices']) > 0:
         caption = response['choices'][0]['text'].strip()
 
         prompt = f"Generate 15 hashtags related to:\n{caption}\n\nHashtags:"
-        hashtags_response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
+        hashtags_response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=100)
         hashtags = [hashtag.strip() for hashtag in hashtags_response['choices'][0]['text'].split('\n')]
 
         prompt = f"Generate location suggestion for:\n{caption}\n\nLocation:"
-        location_response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=50)
+        location_response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=50)
         location = location_response['choices'][0]['text'].strip()
 
         prompt = f"Generate tag people suggestions for:\n{caption}\n\nTag people:"
-        tag_people_response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
+        tag_people_response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=100)
         tag_people = [person.strip() for person in tag_people_response['choices'][0]['text'].split('\n')]
 
         return caption, hashtags, location, tag_people
@@ -43,8 +46,14 @@ def index(request):
                         elem.extract()
                     main_text = soup.get_text()
                     main_text = main_text.strip()
-                    
-                    caption, hashtags, location, tag_people = content(main_text)
+                    if len(main_text) > 10000:
+                        strip_text = main_text[100:9000]
+                        # strip_text.strip()
+                        # print("##",strip_text,len(strip_text))
+                        caption, hashtags, location, tag_people = content(strip_text)
+                    else:
+                        caption, hashtags, location, tag_people = content(main_text)
+
                     
                     # print(type(hashtags),"hashtags")
                     # print(type(tag_people),"tag_people")
